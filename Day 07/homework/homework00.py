51174d16b0d414731deb5f483c553b468d811598
edisherashvilitazo <tazoedisherashvili6@gmail.com>
2024-05-27 20:37:03 +0400

    commit

---
+++ Day 07/homework/homework00.py
@@ -0,0 +1,7 @@
+# მომხმარებელს შეეკითხეთ მისი ასაკი, ასევე შეეკითხეთ რამდენი წლით სურს დროში მოგზაურობა 
+#და დაპრინტეთ რამდენი წლის იქნება დროში მოგზაურობის შემდეგ.
+
+user_age = int(input("Enter your age: "))
+time_travel = int(input("Enter how many years into the future would you like to travel: ")) 
+
+print("After time traveling, your age will be", user_age  + time_travel)
---
+++ Day 08/homework/homework00.py
@@ -0,0 +1,7 @@
+#მომხმარებელს შემოატაინეთ თავისი ნიშნები(რიცხვები), შემდეგ კი გამოითვალეთ მისი საშუალო არითმეტიკული
+
+grade1 = int(input("Enter first grade: "))
+grade2 = int(input("Enter second grade: "))
+grade3 = int(input("Enter thrird grade: "))
+
+print("Arithmetic average of your grades is", (grade1 + grade2 + grade3) / 3)
