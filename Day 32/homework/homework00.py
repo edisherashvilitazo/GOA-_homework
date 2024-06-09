7d34c352fbffffd81693c55e15ee36e30900b542
edisherashvilitazo <tazoedisherashvili6@gmail.com>
2024-06-09 14:30:53 +0400

    commit

--- Day 32/homework/homework00.py
+++
@@ -1,8 +0,0 @@
-# შექმენით სია სადაც შეიყვანთ თქვენი ოჯახის წევრების სახელებს,შემდეგ კი დაბეჭდეთ თითოეული მათგანი ცალცალკე.
-
-family = ["Lika", "Gio", "Mari", "Tazo"]
-
-print(family[0])
-print(family[1])
-print(family[2])
-print(family[3])
---
+++ Day 34/homework.py
@@ -0,0 +1,8 @@
+my_cars = ("BMW", "bughaty", "merceders", "lamborgin")
+my_cars[4] = "bugaty"
+my_cars[5] = "audi"
+my_cars[6] = "lexsus"
+my_cars[7] = "BMW"
+
+print(my_cars)
+print(my_cars[0]+" " +my_cars[1]+" " +[2]+" " +my_cars[3])
